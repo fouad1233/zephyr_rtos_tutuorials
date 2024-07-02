8af8d8eccac70d50cb674fef324a7e1bf5d29c98
Fouad Aladhami <fouadiadhami@gmail.com>
2024-07-02 13:38:47 +0300

    button configurations added to the overlay file

--- 003_gpio_interrupt/src/main.c
+++ 003_gpio_interrupt/src/main.c
@@ -5,12 +5,9 @@
 
 #define GPIO_PIN 13
 #define LED0_NODE DT_NODELABEL(blinking_led)
-int counter = 30;
-//#if DT_NODE_HAS_STATUS(MY_SERIAL, okay)
+int counter = 0;
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
-//#else
-//#error "Node is disabled"
-//#endif
+
 
 void increment_counter(void);
 int main(void){
@@ -23,7 +20,7 @@ int main(void){
     gpio_conf = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
     //GPIO_OUTPUT_ACTIVE (or GPIO_OUTPUT_INACTIVE): Configures a GPIO pin
     // as an output and sets its initial state to the active (or inactive) state.
-    counter = counter + 1;
+
 
     int pin_set = 0;
     while(true){
