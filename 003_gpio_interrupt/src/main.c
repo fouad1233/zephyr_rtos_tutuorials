69bd176e9579a3b27243b633742c495fe2a4a590
Fouad Aladhami <fouadiadhami@gmail.com>
2024-07-02 13:59:09 +0300

    main.c updated, project runs success
    - The project is test and it run success

--- 003_gpio_interrupt/src/main.c
+++ 003_gpio_interrupt/src/main.c
@@ -5,36 +5,60 @@
 
 #define GPIO_PIN 13
 #define LED0_NODE DT_NODELABEL(blinking_led)
+
+
+/*Function prototypes*/
+void increment_counter(void);
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins);
+
+/*Variables*/
 int counter = 0;
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(DT_NODELABEL(my_button), gpios);
+static struct gpio_callback button_cb_data;
 
 
-void increment_counter(void);
 int main(void){
     if (!device_is_ready(led.port)) {
-        //printk("GPIO device %s is not ready\n", gpio_ct_dev->name);
         return 1;
     }
-    int gpio_conf;
-    //gpio_conf = gpio_pin_configure(led.port,led.pin, led.dt_flags);
-    gpio_conf = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    //GPIO_OUTPUT_ACTIVE (or GPIO_OUTPUT_INACTIVE): Configures a GPIO pin
-    // as an output and sets its initial state to the active (or inactive) state.
+    if (!device_is_ready(button.port)) {
+        return 1;
+    }
+    
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    
+    gpio_pin_configure_dt(&button, GPIO_INPUT);
+
+    //set the interrupts 
+    // Configure the button pin to trigger an interrupt on the rising edge
+    gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE); 
+    // Set up the callback function to be called when the button is pressed
+    gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+    // Enable the interrupt for the button pin
+    gpio_add_callback(button.port, &button_cb_data);
+
+    
 
 
-    int pin_set = 0;
     while(true){
-        increment_counter();
-        //pin_set = gpio_pin_set_raw(led.port, led.pin, 1);
-        //pin_set = gpio_pin_set_dt(&led, 1);
-        pin_set = gpio_pin_toggle_dt(&led);
-        k_msleep(1000); // Sleep for 1 second
-        //pin_set = gpio_pin_set_dt(&led, 0);
-        //k_msleep(1000); // Sleep for 1 second
+        
     }
     return 0;
 }
-void increment_counter(){
+
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins){
+    /* The arguments are 
+    * dev: The device that triggered the interrupt
+    * cb: The callback structure that was triggered
+    * pins: The pin that triggered the interrupt
+    */
+    increment_counter();
+    gpio_pin_toggle_dt(&led);
+}
+
+void increment_counter(void){
     counter = counter + 1;
 }
     
