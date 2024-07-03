217f7941da1e8988d355b64970a2bef60b6253e7
Fouad Aladhami <fouadiadhami@gmail.com>
2024-07-03 16:48:24 +0300

    005 project init

---
+++ 005_bootloader_usage/src/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/devicetree.h>
+#define LED0_NODE DT_NODELABEL(green_led)
+
+
+int counter = 30;
+void increment_counter(void);
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+
+int main(void){
+    
+    gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+
+    
+    while(true){
+        increment_counter();
+        gpio_pin_toggle_dt(&led);
+        k_msleep(1000); // Sleep for 1 second
+        
+    }
+    return 0;
+}
+void increment_counter(){
+    counter = counter + 1;
+}
+    
+
+
+
+
